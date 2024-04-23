42948842358a33ec3e9b341cb38bc897d402695e
LuigiMottola <sputnick427@gmail.com>
2024-04-23 16:37:13 +0200

    Termine esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Questa Ã¨ la somma degli elementi nella prima riga della matrice: " + sommaElementiMatrice());
+        ;    }
 
-        UnicodeFinder finder = new UnicodeFinder();
-        StringBuilder result = finder.unicodeAtIndex("Ciao", 1);
+    public static int sommaElementiMatrice() {
+        int[][] matrice = {{5, 8, 4, 2, 9}, {3, 6, 10, 7}};
+        int sum = 0;
 
-        System.out.println(result);
+        for (int i = 0; i < matrice[0].length ; i++) {
+            sum += matrice[0][i];
+        }
 
-
+        return sum;
     }
 
 }
